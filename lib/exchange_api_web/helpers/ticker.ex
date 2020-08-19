00cb8761b9294cf00d7cbd93b28c077776f42f79
teixeiramarcia <filipa.teixeira98@gmail.com>
2020-08-19 09:41:32 +0100

    Add buttons to switch tickers; table of open orders

---
+++ lib/exchange_api_web/helpers/ticker.ex
@@ -0,0 +1,10 @@
+defmodule ExchangeApiWeb.Ticker do
+
+  def get_ticker(ticker) do
+    case ticker do
+      "AUXLND" -> {:ok, :AUXLND}
+      "AGUS" -> {:ok, :AGUS}
+      _ -> {:error, "Ticker does not exist"}
+    end
+  end
+end
--- lib/exchange_api_web/live/dashboard_live.ex
+++ lib/exchange_api_web/live/dashboard_live.ex
@@ -1,27 +1,60 @@
 defmodule ExchangeApiWeb.DashboardLive do
   use ExchangeApiWeb, :live_view
+  alias ExchangeApiWeb.Ticker
 
   def mount(_params, _session, socket) do
     if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
 
-    {:ok, total_buy_orders_AUXLND} = Exchange.total_buy_orders(:AUXLND)
-    {:ok, total_buy_orders_AGUS} = Exchange.total_buy_orders(:AGUS)
+    {:ok, total_buy_orders} = Exchange.total_buy_orders(:AUXLND)
+    {:ok, total_sell_orders} = Exchange.total_sell_orders(:AUXLND)
+    {:ok, open_orders} = Exchange.open_orders(:AUXLND)
 
     {:ok, assign(socket,
+      ticker: "AUXLND",
       time: NaiveDateTime.local_now(),
-      total_buy_orders_AUXLND: total_buy_orders_AUXLND,
-      total_buy_orders_AGUS: total_buy_orders_AGUS
+      total_buy_orders: total_buy_orders,
+      total_sell_orders: total_sell_orders,
+      open_orders: open_orders
     )}
   end
 
   def handle_info(:tick, socket) do
-    {:ok, total_buy_orders_AUXLND} = Exchange.total_buy_orders(:AUXLND)
-    {:ok, total_buy_orders_AGUS} = Exchange.total_buy_orders(:AGUS)
+    {:ok, ticker} = Ticker.get_ticker(socket.assigns.ticker)
+    {:ok, total_buy_orders} = Exchange.total_buy_orders(ticker)
+    {:ok, total_sell_orders} = Exchange.total_sell_orders(:AUXLND)
+    {:ok, open_orders} = Exchange.open_orders(ticker)
 
     {:noreply, assign(socket,
       time: NaiveDateTime.local_now(),
-      total_buy_orders_AUXLND: total_buy_orders_AUXLND,
-      total_buy_orders_AGUS: total_buy_orders_AGUS
+      total_buy_orders: total_buy_orders,
+      total_sell_orders: total_sell_orders,
+      open_orders: open_orders
+    )}
+  end
+
+  def handle_event("AUXLND", _, socket) do
+    {:ok, total_buy_orders} = Exchange.total_buy_orders(:AUXLND)
+    {:ok, total_sell_orders} = Exchange.total_sell_orders(:AUXLND)
+    {:ok, open_orders} = Exchange.open_orders(:AUXLND)
+
+    {:noreply, assign(socket,
+      ticker: "AUXLND",
+      total_buy_orders: total_buy_orders,
+      total_sell_orders: total_sell_orders,
+      open_orders: open_orders
+    )}
+  end
+
+  def handle_event("AGUS", _, socket) do
+    {:ok, total_buy_orders} = Exchange.total_buy_orders(:AGUS)
+    {:ok, total_sell_orders} = Exchange.total_sell_orders(:AUXLND)
+    {:ok, open_orders} = Exchange.open_orders(:AUXLND)
+
+    {:noreply, assign(socket,
+      ticker: "AGUS",
+      total_buy_orders: total_buy_orders,
+      total_sell_orders: total_sell_orders,
+      open_orders: open_orders
     )}
   end
 end
