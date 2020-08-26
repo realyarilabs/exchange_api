26db0e2f952e08a9657f65a547fa5938eea556ab
teixeiramarcia <filipa.teixeira98@gmail.com>
2020-08-26 10:03:30 +0100

    Add last price and list size to order book

--- lib/exchange_api_web/live/order_book_live.ex
+++ lib/exchange_api_web/live/order_book_live.ex
@@ -7,10 +7,10 @@ defmodule ExchangeApiWeb.OrderBookLive do
 
     {:ok, tick} = Ticker.get_ticker(ticker)
     {:ok, open_orders} = Exchange.open_orders(tick)
-    # {:ok, last_price_sell} = Exchange.last_price(tick, :sell)
-    # {:ok, last_price_buy} = Exchange.last_price(tick, :buy)
-    # {:ok, last_size_sell} = Exchange.last_size(tick, :sell)
-    # {:ok, last_size_buy} = Exchange.last_size(tick, :buy)
+    {:ok, last_price_sell} = Exchange.last_price(tick, :sell)
+    {:ok, last_price_buy} = Exchange.last_price(tick, :buy)
+    {:ok, last_size_sell} = Exchange.last_size(tick, :sell)
+    {:ok, last_size_buy} = Exchange.last_size(tick, :buy)
 
     sell_orders = open_orders |> Enum.filter(fn order -> order.side == :sell end)
     buy_orders = open_orders |> Enum.filter(fn order -> order.side == :buy end)
@@ -18,10 +18,10 @@ defmodule ExchangeApiWeb.OrderBookLive do
     {:ok,
      assign(socket,
        ticker: ticker,
-      #  last_price_sell: last_price_sell,
-      #  last_price_buy: last_price_buy,
-      #  last_size_sell: last_size_sell,
-      #  last_size_buy: last_size_buy,
+       last_price_sell: last_price_sell,
+       last_price_buy: last_price_buy,
+       last_size_sell: last_size_sell,
+       last_size_buy: last_size_buy,
        sell_orders: sell_orders,
        buy_orders: buy_orders
      )}
@@ -30,20 +30,20 @@ defmodule ExchangeApiWeb.OrderBookLive do
   def handle_info(:tick, socket) do
     {:ok, ticker} = Ticker.get_ticker(socket.assigns.ticker)
     {:ok, open_orders} = Exchange.open_orders(ticker)
-    # {:ok, last_price_sell} = Exchange.last_price(ticker, :sell)
-    # {:ok, last_price_buy} = Exchange.last_price(ticker, :buy)
-    # {:ok, last_size_sell} = Exchange.last_size(ticker, :sell)
-    # {:ok, last_size_buy} = Exchange.last_size(ticker, :buy)
+    {:ok, last_price_sell} = Exchange.last_price(ticker, :sell)
+    {:ok, last_price_buy} = Exchange.last_price(ticker, :buy)
+    {:ok, last_size_sell} = Exchange.last_size(ticker, :sell)
+    {:ok, last_size_buy} = Exchange.last_size(ticker, :buy)
 
     sell_orders = open_orders |> Enum.filter(fn order -> order.side == :sell end)
     buy_orders = open_orders |> Enum.filter(fn order -> order.side == :buy end)
 
     {:noreply,
      assign(socket,
-      #  last_price_sell: last_price_sell,
-      #  last_price_buy: last_price_buy,
-      #  last_size_sell: last_size_sell,
-      #  last_size_buy: last_size_buy,
+       last_price_sell: last_price_sell,
+       last_price_buy: last_price_buy,
+       last_size_sell: last_size_sell,
+       last_size_buy: last_size_buy,
        sell_orders: sell_orders,
        buy_orders: buy_orders
      )}
