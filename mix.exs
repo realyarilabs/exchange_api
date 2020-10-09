4aeb8604a286c51186e6e4535fb80d7be7aeabcc
Michael Agra <agramichael@gmail.com>
2020-10-09 11:58:21 +0100

    update elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExchangeApi.MixProject do
     [
       app: :exchange_api,
       version: "0.0.2",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
